99a3ebc71e9debca4f75fd3819bcae6e136548ac
Christian Thiele <post@thiele-christian.de>
2022-02-09 23:23:16 +0100

    fixed string to enum in transferObject constructor

--- datahub_common/lib/src/api/dto/transfer_object.dart
+++ datahub_common/lib/src/api/dto/transfer_object.dart
@@ -12,7 +12,7 @@ abstract class TransferObject {
         .map((e) => Tuple(
             dataFields.firstOrNullWhere((p0) => p0.key == e.key), e.value))
         .where((element) => element.a != null)) {
-      set(e.a!, e.b);
+      set(e.a!, decodeTyped(e.b));
     }
   }
 
