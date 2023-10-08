d2b779919c709d81ae67c68c4465c798b3122bb1
Christian Thiele <post@thiele-christian.de>
2023-10-08 14:27:29 +0200

    0.17.0-dev.47

--- lib/src/broker/amqp/amqp_broker_service.dart
+++ lib/src/broker/amqp/amqp_broker_service.dart
@@ -10,7 +10,7 @@ import '../broker_service.dart';
 /// Configuration values:
 ///   `host`: Broker connection host
 ///   `port`: Broker connection port (optional, default 5672)
-///   `user`: Username for authentication at broker
+///   `username`: Username for authentication at broker
 ///   `password`: Password for authentication at broker
 ///   `heartbeatPeriod`: Milliseconds between heartbeats (optional, default 5000)
 class AmqpBrokerService extends BrokerService {
@@ -18,7 +18,7 @@ class AmqpBrokerService extends BrokerService {
 
   late final _configHost = config<String>('host');
   late final _configPort = config<int?>('port') ?? 5672;
-  late final _configUser = config<String>('user');
+  late final _configUser = config<String>('username');
   late final _configPassword = config<String>('password');
   late final _heartbeatPeriod = config<int?>('heartbeatPeriod') ?? 3000;
   late final _connectionName = config<String?>('connectionName');
