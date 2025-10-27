54f70ef55a1410808d0ff0a8c5ad2901ffbe1ce2
Christian Thiele <post@thiele-christian.de>
2025-10-27 21:14:54 +0100

    [datahub_postgres] config vars for PostgresqlDataRepositoryService

--- packages/datahub_postgres/lib/src/data/postgresql_data_repository_service.dart
+++ packages/datahub_postgres/lib/src/data/postgresql_data_repository_service.dart
@@ -1,6 +1,6 @@
-import 'package:datahub/data.dart';
-import 'package:datahub/scaffold.dart';
+import 'package:datahub/datahub.dart';
 import 'package:datahub_postgres/data.dart';
+import 'package:datahub_postgres/services.dart';
 
 /// Standalone DataRepository providing direct access to the postgres data.
 ///
@@ -8,9 +8,17 @@ import 'package:datahub_postgres/data.dart';
 /// as mixin for a [ServiceInstance] to override methods.
 class PostgresqlDataRepositoryService<TData extends DataObject<TData>>
     implements Service {
+  final Find<Postgresql> postgresql;
+  final Config<String> schemaName;
+  final Config<String?> relationName;
   final DataBean<TData> bean;
 
-  PostgresqlDataRepositoryService({required this.bean});
+  PostgresqlDataRepositoryService({
+    required this.bean,
+    this.postgresql = const Find(),
+    this.schemaName = const Config('schemaName', defaultValue: 'public'),
+    this.relationName = const Config('relationName'),
+  });
 
   @override
   ServiceInstance<PostgresqlDataRepositoryService> createInstance() =>
@@ -24,6 +32,15 @@ class _PostgresqlDataRepositoryServiceInstance<TData extends DataObject<TData>>
           PostgresqlDataRepositoryService<TData>,
           TData
         > {
+  @override
+  Find<Postgresql> get postgresql => service.postgresql;
+
+  @override
+  Config<String> get schemaName => service.schemaName;
+
+  @override
+  Config<String?> get relationName => service.relationName;
+
   @override
   DataBean<TData> get bean => service.bean;
 }
