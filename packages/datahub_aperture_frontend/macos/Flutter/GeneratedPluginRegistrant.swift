8710004be294ee983c9c0deac8e652f508bfb3b5
Christian Thiele <post@thiele-christian.de>
2025-11-27 11:18:22 +0100

    [datahub_aperture_frontend] fixed macos build

--- packages/datahub_aperture_frontend/macos/Flutter/GeneratedPluginRegistrant.swift
+++ packages/datahub_aperture_frontend/macos/Flutter/GeneratedPluginRegistrant.swift
@@ -5,12 +5,14 @@
 import FlutterMacOS
 import Foundation
 
+import app_links
 import file_picker
 import path_provider_foundation
 import shared_preferences_foundation
 import url_launcher_macos
 
 func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
+  AppLinksMacosPlugin.register(with: registry.registrar(forPlugin: "AppLinksMacosPlugin"))
   FilePickerPlugin.register(with: registry.registrar(forPlugin: "FilePickerPlugin"))
   PathProviderPlugin.register(with: registry.registrar(forPlugin: "PathProviderPlugin"))
   SharedPreferencesPlugin.register(with: registry.registrar(forPlugin: "SharedPreferencesPlugin"))
